7de670f7ef0f3521010c6212d8bc91d39452525f
John Kenneth Ganzan <jkganzan13@gmail.com>
2018-09-30 16:32:47 +1300

    Add mobile filters

--- app/components/common/List/Styled.js
+++ app/components/common/List/Styled.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
+import { Button } from 'reactstrap';
 import { DarkYellow, LightGrey } from 'assets/styles/colors';
-
-const filterWidth = 356;
 
 export const Container = styled.div`
   background-color: transparent;
@@ -26,4 +25,37 @@ export const ItemsContainer = styled.div`
   flex: 1;
   height: calc(100vh - 82px);
   overflow: auto;
+`;
+
+export const FilterBtn = styled(Button)`
+  align-items: center;
+  background: transparent;
+  border: 0;
+  color: ${LightGrey};
+  display: flex;
+  font-size: 20px;
+  justify-content: center;
+  padding: 4px 8px;
+  margin-bottom: 8px;
+
+  &:active,
+  &:active:focus,
+  &:focus,
+  &:hover {
+    background-color: transparent !important;
+    box-shadow: none !important;
+  }
+
+  svg {
+    fill: none;
+    margin-right: 4px;
+  }
+`;
+
+export const Drawer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
 `;
--- app/components/common/List/index.js
+++ app/components/common/List/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FiFilter } from 'react-icons/fi';
+import { Mobile } from 'components/common/Responsive';
 import Pager from './Pager';
 import * as Styled from './Styled';
+
+const FilterButton = () => (
+  <Styled.FilterBtn>
+    <FiFilter /> Filters
+  </Styled.FilterBtn>
+);
 
 const List = props => (
   <Styled.Container>
     {props.renderFilter && props.renderFilter()}
     <Styled.ItemsContainer>
+      <Mobile>{props.renderFilter && <FilterButton />}</Mobile>
       {props.dataSource.map(props.renderItem)}
       {props.pagination && <Pager {...props.pagination} />}
     </Styled.ItemsContainer>
